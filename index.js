f1fc9415921be465282915154ba6306911ccc8a5
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-15 18:28:29 +0100

    warnings

--- index.js
+++ index.js
@@ -8,25 +8,40 @@ window.addEventListener("DOMContentLoaded", () => {
   const resetBtn = document.querySelector(".reset-button");
   const displayTestBtn = document.querySelector(".test-text-button");
 
+  const errorMsg = document.querySelector("#error-message");
+
   const originalHTLM = testTextEl.innerHTML; //Original text version
 
   //Display test text button
 
   displayTestBtn.addEventListener("click", () => {
     textContainer.classList.toggle("show");
-
-    if (textContainer.classList.contains("show")) {
-      displayTestBtn.textContent = "Hide text";
-    } else {
-      displayTestBtn.textContent = "Display a test text";
-    }
+    displayTestBtn.textContent = textContainer.classList.contains("show")
+      ? "Hide text"
+      : "Display a test text";
   });
 
   //Search logic
 
   inputQ.addEventListener("keydown", (event) => {
+    errorMsg.style.display = "none";
+
     if (event.key === "Enter") {
       let wordSearch = inputQ.value.trim();
+
+      const isTextVisible = textContainer.classList.contains("show");
+
+      if (!isTextVisible) {
+        errorMsg.textContent = "⚠️ Please display the text first!";
+        errorMsg.style.display = "block";
+        errorMsg.style.color = "#e67e22";
+        errorMsg.style.borderColor = "#e67e22";
+        errorMsg.style.backgroundColor = "#fff5e6";
+
+        inputQ.value = "";
+        inputQ.focus();
+        return;
+      }
 
       if (wordSearch !== "") {
         const regExp = new RegExp(wordSearch, "gi");
@@ -37,12 +52,13 @@ window.addEventListener("DOMContentLoaded", () => {
             "<span class='word-search-span'>$&</span>",
           );
         } else {
-          alert(`The word ${wordSearch} was not found in the text`);
-          inputQ.value = "";
+          errorMsg.textContent = "Word not found!";
+          errorMsg.style.color = "red";
+          errorMsg.style.display = "block";
           testTextEl.innerHTML = originalHTLM;
+          inputQ.value = "";
+          inputQ.focus();
         }
-      } else {
-        testTextEl.innerHTML = originalHTLM;
       }
     }
   });
@@ -52,6 +68,7 @@ window.addEventListener("DOMContentLoaded", () => {
   resetBtn.addEventListener("click", () => {
     inputQ.value = "";
     testTextEl.innerHTML = originalHTLM;
+    errorMsg.style.display = "none";
     console.log("App reseted!");
   });
 });
