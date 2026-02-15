e27c888c1e9a486c5f6679a865091bb31ac794fb
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-15 17:52:35 +0100

    text

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 window.addEventListener("DOMContentLoaded", () => {
   const inputQ = document.querySelector("#word-search");
-  const textQ = document.querySelector("#text");
+  const textQ = document.querySelector("#test-text");
+
+  const textContainer = document.querySelector(".text-container");
+
   const resetBtn = document.querySelector(".reset-button");
+  const displayTestBtn = document.querySelector(".test-text-button");
 
   const originalText = textQ.innerText;
+
+  displayTestBtn.addEventListener("click", () => {
+    textContainer.classList.toggle("show");
+
+    if (textContainer.classList.contains("show")) {
+      displayTestBtn.textContent = "Hide text";
+    } else {
+      displayTestBtn.textContent = "Display a test text";
+    }
+
+    displayTestBtn.textContainer = "Text Displayed!";
+    displayTestBtn.disabled = true;
+  });
 
   //Search logical
   inputQ.addEventListener("keydown", (event) => {
