14fac6d4a3a950a1592fe7dc9df20f77a05be086
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-15 14:18:32 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  let inputQ = document.querySelector("#word-search");
+  let textQ = document.querySelector("#text");
+
+  inputQ.addEventListener("keydown", (event) => {
+    let word = inputQ.value;
+  });
+});
